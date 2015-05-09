fd230562ba15e1a14ef8837fb75dbe82c3f6fe92
Stylon Wang <stylon.wang@avermedia.com>
2015-05-09 22:11:00 +0800

    binary message from user to kernel works, too.
    	modified:   Makefile
    	modified:   kernel.c
    	new file:   user2.c

--- v1/kernel.c
+++ v1/kernel.c
@@ -2,26 +2,15 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 
-/* attributes (variables): the index in this enum is used as a reference for the type,
- *             userspace application has to indicate the corresponding type
- *             the policy is used for security considerations 
- */
-enum {
-	N_ATTR_UNSPECIFIED,
-	N_ATTR_MSG1,
-//	N_ATTR_MSG2,
-//	N_ATTR_MSG3,
-    __N_ATTR_MAX,
-};
-#define N_ATTR_MAX (__N_ATTR_MAX - 1)
+#include "cmd.h"
 
 /* attribute policy: defines which attribute has which type (e.g int, char * etc)
  * possible values defined in net/netlink.h 
  */
 static struct nla_policy N_genl_policy[N_ATTR_MAX + 1] = {
 	[N_ATTR_MSG1] = { .type = NLA_NUL_STRING },
-//	[N_ATTR_MSG2] = { .type = NLA_U16 },
-//	[N_ATTR_MSG3] = { .type = NLA_U32 },
+	[N_ATTR_MSG2] = { .type = NLA_BINARY, .len = sizeof(struct N_message) },
+	[N_ATTR_MSG3] = { .type = NLA_U32 },
 };
 
 #define VERSION_NR 1
@@ -33,16 +22,6 @@ static struct genl_family N_gnl_family = {
 	.version = VERSION_NR,                   //version number  
 	.maxattr = N_ATTR_MAX,
 };
-
-/* commands: enumeration of all commands (functions), 
- * used by userspace application to identify command to be ececuted
- */
-enum {
-	N_CMD_UNSPEC,
-	N_CMD_ECHO,
-	__N_CMD_MAX,
-};
-#define N_CMD_MAX (__N_CMD_MAX - 1)
 
 /* an echo command, receives a message, prints it and sends another message back */
 int doc_exmpl_echo(struct sk_buff *skb_2, struct genl_info *info)
@@ -106,34 +85,113 @@ int doc_exmpl_echo(struct sk_buff *skb_2, struct genl_info *info)
         goto out;
     return 0;
 
- out:
+out:
     printk("an error occured in doc_exmpl_echo:\n");
   
     return 0;
 }
+
+int doc_exmpl_echo_bin(struct sk_buff *skb_2, struct genl_info *info)
+{
+    struct nlattr *na;
+    struct sk_buff *skb;
+    int rc;
+	void *msg_head;
+	struct N_message *nmsg;
+
+    printk("%s +\n", __FUNCTION__);
+	
+    if (info == NULL)
+            goto out;
+
+    /*for each attribute there is an index in info->attrs which points to a nlattr structure
+     *in this structure the data is given
+     */
+    na = info->attrs[N_ATTR_MSG2];
+   	if (na) {
+        nmsg = (struct N_message *)nla_data(na);
+        if (nmsg == NULL)
+            printk("error while receiving data\n");
+        else
+            printk("received msg from %s, event %d param[5]=%d\n", nmsg->sender, nmsg->event, nmsg->param[N_PARAM_MAX-1]);
+	}
+    else
+	    printk("no info->attrs %i\n", N_ATTR_MSG1);
+
+    /* send a message back*/
+    /* allocate some memory, since the size is not yet known use NLMSG_GOODSIZE*/	
+    skb = genlmsg_new(NLMSG_GOODSIZE, GFP_KERNEL);
+    if (skb == NULL)
+        goto out;
+
+    /* create the message headers */
+    /* arguments of genlmsg_put: 
+       struct sk_buff *, 
+       int (sending) pid, 
+       int sequence number, 
+       struct genl_family *, 
+       int flags, 
+       u8 command index (why do we need this?)
+    */
+   	msg_head = genlmsg_put(skb, 0, info->snd_seq+1, &N_gnl_family, 0, N_CMD_ECHO);
+    if (msg_head == NULL) {
+        rc = -ENOMEM;
+        goto out;
+    }
+    /* add a DOC_EXMPL_A_MSG attribute (actual value to be sent) */
+    rc = nla_put_string(skb, N_ATTR_MSG1, "hello world from kernel space");
+    if (rc != 0)
+        goto out;
+
+    /* finalize the message */
+    genlmsg_end(skb, msg_head);
+
+    /* send the message back */
+    rc = genlmsg_unicast(genl_info_net(info), skb,info->snd_pid );
+    if (rc != 0)
+        goto out;
+    return 0;
+out:
+    printk("an error occured in doc_exmpl_echo_bin:\n");
+
+    return 0;
+}
+
 /* commands: mapping between the command enumeration and the actual function*/
-struct genl_ops N_gnl_ops_echo = {
-	.cmd = N_CMD_ECHO,
-	.flags = 0,
-	.policy = N_genl_policy,
-	.doit = doc_exmpl_echo,
-	.dumpit = NULL,
+
+struct genl_ops N_gnl_ops[] = {
+    {
+        .cmd = N_CMD_ECHO,
+        .flags = 0,
+        .policy = N_genl_policy,
+        .doit = doc_exmpl_echo,
+        .dumpit = NULL,
+    },
+
+    {
+        .cmd = N_CMD_ECHO_BIN,
+        .flags = 0,
+        .policy = N_genl_policy,
+        .doit = doc_exmpl_echo_bin,
+        .dumpit = NULL,
+    },
 };
 
 static int __init kernel_init(void)
 {
 	int rc;
     printk("INIT genetlink\n");
-        
+#if 0        
     /*register new family*/
 	rc = genl_register_family(&N_gnl_family);
 	if (rc != 0)
 		goto failure;
+#endif //0
 
     /*register functions (commands) of the new family*/
-	rc = genl_register_ops(&N_gnl_family, &N_gnl_ops_echo);
+    rc = genl_register_family_with_ops(&N_gnl_family, N_gnl_ops, sizeof(N_gnl_ops)/sizeof(N_gnl_ops[0]));
 	if (rc != 0){
-        printk("register ops: %i\n",rc);
+        printk("register ops failed: %i\n",rc);
         genl_unregister_family(&N_gnl_family);
 		goto failure;
     }
@@ -148,17 +206,27 @@ failure:
 static void __exit kernel_exit(void)
 {
     int ret;
+    size_t s;
     printk("EXIT genetlink\n");
+    #if 0
     /*unregister the functions*/
 	ret = genl_unregister_ops(&N_gnl_family, &N_gnl_ops_echo);
 	if(ret != 0){
         printk("unregister ops: %i\n",ret);
         return;
     }
+    #endif //0
+    for(s=0; s<sizeof(N_gnl_ops)/sizeof(N_gnl_ops[0]); ++s) {
+        ret = genl_unregister_ops(&N_gnl_family, &N_gnl_ops[s]);
+        if(ret != 0){
+            printk("unregister ops %d failed: %i\n", (int)s, ret);
+        }
+    }
+
     /*unregister the family*/
 	ret = genl_unregister_family(&N_gnl_family);
 	if(ret !=0){
-        printk("unregister family %i\n",ret);
+        printk("unregister family failed: %i\n",ret);
     }
 }
 
---
+++ v1/user2.c
@@ -0,0 +1,225 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <poll.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <signal.h>
+
+#include <linux/genetlink.h>
+
+#include "cmd.h"
+
+/*
+ * Generic macros for dealing with netlink sockets. Might be duplicated
+ * elsewhere. It is recommended that commercial grade applications use
+ * libnl or libnetlink and use the interfaces provided by the library
+ */
+#define GENLMSG_DATA(glh) ((void *)(NLMSG_DATA(glh) + GENL_HDRLEN))
+#define GENLMSG_PAYLOAD(glh) (NLMSG_PAYLOAD(glh, 0) - GENL_HDRLEN)
+#define NLA_DATA(na) ((void *)((char*)(na) + NLA_HDRLEN))
+//#define NLA_PAYLOAD(len) (len - NLA_HDRLEN)
+
+int nl_sd;                      /*the socket */
+/*
+ * Create a raw netlink socket and bind
+ */
+static int create_nl_socket(int protocol, int groups)
+{
+    socklen_t addr_len;
+    int fd;
+    struct sockaddr_nl local;
+
+    fd = socket(AF_NETLINK, SOCK_RAW, protocol);
+    if (fd < 0) {
+            perror("socket");
+            return -1;
+    }
+
+    memset(&local, 0, sizeof(local));
+    local.nl_family = AF_NETLINK;
+    local.nl_groups = groups;
+    if (bind(fd, (struct sockaddr *)&local, sizeof(local)) < 0)
+            goto error;
+
+    return fd;
+ error:
+    close(fd);
+    return -1;
+}
+
+/*
+ * Send netlink message to kernel
+ */
+int sendto_fd(int s, const char *buf, int bufLen)
+{
+    struct sockaddr_nl nladdr;
+    int r;
+
+    memset(&nladdr, 0, sizeof(nladdr));
+    nladdr.nl_family = AF_NETLINK;
+
+    while ((r = sendto(s, buf, bufLen, 0, (struct sockaddr *)&nladdr,
+                       sizeof(nladdr))) < bufLen) {
+        if (r > 0) {
+            buf += r;
+            bufLen -= r;
+        } else if (errno != EAGAIN)
+             return -1;
+    }
+    return 0;
+}
+
+/*
+ * Probe the controller in genetlink to find the family id
+ * for the CONTROL_EXMPL family
+ */
+int get_family_id(int sd)
+{
+    struct {
+            struct nlmsghdr n;
+            struct genlmsghdr g;
+            char buf[256];
+    } family_req;
+
+    struct {
+            struct nlmsghdr n;
+            struct genlmsghdr g;
+            char buf[256];
+    } ans;
+
+    int id;
+    struct nlattr *na;
+    int rep_len;
+
+    /* Get family name */
+    family_req.n.nlmsg_type = GENL_ID_CTRL;
+    family_req.n.nlmsg_flags = NLM_F_REQUEST;
+    family_req.n.nlmsg_seq = 0;
+    family_req.n.nlmsg_pid = getpid();
+    family_req.n.nlmsg_len = NLMSG_LENGTH(GENL_HDRLEN);
+    family_req.g.cmd = CTRL_CMD_GETFAMILY;
+    family_req.g.version = 0x1;
+
+    na = (struct nlattr *)GENLMSG_DATA(&family_req);
+    na->nla_type = CTRL_ATTR_FAMILY_NAME;
+    /*------change here--------*/
+    na->nla_len = strlen("NOTIFIER_HUB") + 1 + NLA_HDRLEN;
+    strcpy(NLA_DATA(na), "NOTIFIER_HUB");
+
+    family_req.n.nlmsg_len += NLMSG_ALIGN(na->nla_len);
+
+    if (sendto_fd(sd, (char *)&family_req, family_req.n.nlmsg_len) < 0)
+            return -1;
+
+    rep_len = recv(sd, &ans, sizeof(ans), 0);
+    if (rep_len < 0) {
+            perror("recv");
+            return -1;
+    }
+
+    /* Validate response message */
+    if (!NLMSG_OK((&ans.n), rep_len)) {
+            fprintf(stderr, "invalid reply message\n");
+            return -1;
+    }
+
+    if (ans.n.nlmsg_type == NLMSG_ERROR) {  /* error */
+            fprintf(stderr, "received error\n");
+            return -1;
+    }
+
+    na = (struct nlattr *)GENLMSG_DATA(&ans);
+    na = (struct nlattr *)((char *)na + NLA_ALIGN(na->nla_len));
+    if (na->nla_type == CTRL_ATTR_FAMILY_ID) {
+            id = *(__u16 *) NLA_DATA(na);
+    }
+    return id;
+}
+
+int main()
+{
+   nl_sd = create_nl_socket(NETLINK_GENERIC, 0);
+   if (nl_sd < 0) {
+           printf("create failure\n");
+           return 0;
+   }
+   int id = get_family_id(nl_sd);
+   struct {
+           struct nlmsghdr n;
+           struct genlmsghdr g;
+           char buf[256];
+   } ans;
+
+   struct {
+           struct nlmsghdr n;
+           struct genlmsghdr g;
+           char buf[256];
+   } req;
+   struct nlattr *na;
+
+   printf("family id is %d\n", id);
+   printf("total header size=%d\n", sizeof(struct nlmsghdr)+sizeof(struct genlmsghdr));
+
+   /* Send command needed */
+   req.n.nlmsg_len = NLMSG_LENGTH(GENL_HDRLEN);
+   req.n.nlmsg_type = id;
+   req.n.nlmsg_flags = NLM_F_REQUEST;
+   req.n.nlmsg_seq = 60;
+   req.n.nlmsg_pid = getpid();
+   req.g.cmd = N_CMD_ECHO_BIN;          //N_CMD_ECHO_BIN;
+
+   /*compose message */
+   na = (struct nlattr *)GENLMSG_DATA(&req);
+   na->nla_type = N_ATTR_MSG2;       //N_ATTR_MSG2
+   struct N_message nmsg;
+   memset(&nmsg, 0, sizeof(nmsg));
+   snprintf(nmsg.sender, sizeof(nmsg.sender), "user2");
+   nmsg.event = 3;
+   nmsg.param[N_PARAM_MAX-1] = 4;
+   int mlength = sizeof(nmsg);
+   na->nla_len = mlength + NLA_HDRLEN; //message length
+   memcpy(NLA_DATA(na), &nmsg, mlength);
+   req.n.nlmsg_len += NLMSG_ALIGN(na->nla_len);
+
+   /*send message */
+   struct sockaddr_nl nladdr;
+   int r;
+
+   memset(&nladdr, 0, sizeof(nladdr));
+   nladdr.nl_family = AF_NETLINK;
+
+   r = sendto(nl_sd, (char *)&req, req.n.nlmsg_len, 0,
+              (struct sockaddr *)&nladdr, sizeof(nladdr));
+
+   int rep_len = recv(nl_sd, &ans, sizeof(ans), 0);
+   /* Validate response message */
+   if (ans.n.nlmsg_type == NLMSG_ERROR) {  /* error */
+       struct nlmsgerr *err = (struct nlmsgerr *)NLMSG_DATA(&ans);      
+       printf("error received NACK, error=%d msg type=%d seq=%d, pid=%d - leaving \n", 
+       err->error, err->msg.nlmsg_type, err->msg.nlmsg_seq, err->msg.nlmsg_pid);
+       printf("my pid=%d\n", getpid());
+       return -1;
+   }
+   if (rep_len < 0) {
+           printf("error receiving reply message via Netlink \n");
+           return -1;
+   }
+   if (!NLMSG_OK((&ans.n), rep_len)) {
+           printf("invalid reply message received via Netlink\n");
+           return -1;
+   }
+
+   rep_len = GENLMSG_PAYLOAD(&ans.n);
+   /*parse reply message */
+   na = (struct nlattr *)GENLMSG_DATA(&ans);
+   char *result = (char *)NLA_DATA(na);
+   printf("kernel says: %s\n", result);
+
+   close(nl_sd);
+}
